c4ae9b3e7acf3a47aa8acb4b4800a651cd0df1ef
Matheus Duarte Albuquerque <90364847+Mathdal002@users.noreply.github.com>
2023-11-17 17:23:07 -0300

    Add files via upload

---
+++ desafioPicpay/src/test/java/br/gov/go/sefaz/desafioPicpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.go.sefaz.desafioPicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
